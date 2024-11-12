544dbbe5cf7315dec022c65d122931a374b5d248
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-11 19:45:14 -0500

    feat: ✨ implemented common api for databases
    used to make database calles based on strategy

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -14,3 +14,23 @@ export interface DatabaseStrategy<T> {
 	exec(query: string): Promise<QueryResult>;
 	close(): Promise<void>;
 }
+
+export class DatabaseContext<T> {
+	private dbStrategy!: DatabaseStrategy<T>;
+
+	setStrategy(strategy: DatabaseStrategy<T>) {
+		this.dbStrategy = strategy;
+	}
+
+	async init() {
+		await this.dbStrategy.init();
+	}
+
+	async exec(query: string): Promise<QueryResult> {
+		return await this.dbStrategy.exec(query);
+	}
+
+	async close(): Promise<void> {
+		this.dbStrategy.close();
+	}
+}
