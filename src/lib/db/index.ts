e924f5eee64870c3bba0976158b41921db5ea716
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-29 09:45:21 -0500

    feat: 🏷️ created types used by worker

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,3 @@
-export type DBOptions = Record<string, string | boolean | number>;
-
 export type QueryResult = {
 	data?: unknown;
 	error?: string;
@@ -14,3 +12,36 @@ export interface DatabaseStrategy {
 	exec(query: string): Promise<QueryResult>;
 	close(): Promise<void>;
 }
+
+export type DBOptions = {
+	engine: 'pgsql' | 'sqlite' | 'duckdb';
+	dbName: string;
+	persistent: boolean;
+};
+
+type DBManagerCommands =
+	| 'GET_ACTIVE_DBS'
+	| 'GET_AVAILABLE_DBS'
+	| 'CREATE_DB'
+	| 'LOAD_DB'
+	| 'EXEC_QUERY'
+	| 'CLOSE_DB';
+
+type CommandWithoutArgs<C extends DBManagerCommands> = { command: C };
+type CommandWithArgs<C extends DBManagerCommands, A> = { command: C; args: A };
+
+export type DBManagerMessage =
+	| CommandWithoutArgs<'GET_ACTIVE_DBS'>
+	| CommandWithoutArgs<'GET_AVAILABLE_DBS'>
+	| CommandWithArgs<'CREATE_DB', DBOptions>
+	| CommandWithArgs<'LOAD_DB', DBOptions>
+	| CommandWithArgs<'EXEC_QUERY', { dbName: string; query: string }>
+	| CommandWithArgs<'CLOSE_DB', { dbName: string }>;
+
+export type DBManagerResponse =
+	| { status: 'SUCCESS'; command: 'GET_ACTIVE_DBS'; response: { activeDBs: string[] } }
+	| { status: 'SUCCESS'; command: 'GET_AVAILABLE_DBS'; response: { availableDBs: string[] } }
+	| { status: 'SUCCESS'; command: 'CREATE_DB'; response: { dbName: string } }
+	| { status: 'SUCCESS'; command: 'LOAD_DB'; response: { dbName: string } }
+	| { status: 'SUCCESS'; command: 'CLOSE_DB'; response: { dbName: string } }
+	| { status: 'ERROR'; command: string; response: string };
