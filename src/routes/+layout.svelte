7ce2597f63e63f56d5a772630ec41d16de338c7c
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-30 16:30:17 -0500

    revert: ➖ removed `@vercel/speed-insights`
    causes issues in dev and preview server

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.css';
-
-	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
