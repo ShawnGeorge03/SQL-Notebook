4079cc732322e46c18c1f593022ef2f29504e50e
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-30 09:54:49 -0500

    feat: ✨ implemented worker store subscriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { dbWorkerMessagesStore, dbWorkerResponsesStore } from '$lib/db/worker/store';
 	import type { DBWorkerResponse } from '$lib/db/worker/types';
 
 	import { onDestroy, onMount } from 'svelte';
 
 	let sharedWorker: SharedWorker | undefined;
 	let port: MessagePort | undefined;
+	let loading: boolean = $state(true);
+
 	onMount(() => {
 		try {
 			sharedWorker = new SharedWorker(new URL('$lib/db/worker/index.ts', import.meta.url), {
@@ -15,9 +18,16 @@
 			port.start();
 
 			port.onmessage = (event: MessageEvent<DBWorkerResponse>) => {
-				const { status, command, response } = event.data;
-				console.log(status, command, response)
+				if (event.data.status === 'READY') loading = false;
+				dbWorkerResponsesStore.set(event.data);
 			};
+
+			dbWorkerMessagesStore.subscribe(message => {
+				if (!port) return;
+				port.postMessage(message);
+			})
+
+
 		} catch (e) {
 			console.error(e instanceof Error ? e.message : 'Failed to start DB Worker!');
 		}
@@ -27,3 +37,13 @@
 		port?.close();
 	});
 </script>
+
+<div class="container mx-auto">
+	{#if loading}
+		<p>Loading Notebook</p>
+	{:else}
+		<h1 class="mb-6 text-center text-2xl font-bold">Database Manager</h1>
+
+
+	{/if}
+</div>
