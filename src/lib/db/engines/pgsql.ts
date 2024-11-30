f5d278354e64776305f1af311a609fa0bbbd9675
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-30 16:28:11 -0500

    fix: 🐛 removed svelte from `pgsql` engine

--- src/lib/db/engines/pgsql.ts
+++ src/lib/db/engines/pgsql.ts
@@ -1,5 +1,3 @@
-import { browser, dev } from '$app/environment';
-
 import { IdbFs, MemoryFS, PGlite, type PGliteOptions } from '@electric-sql/pglite';
 import type { DBEngineOptions, DBStrategy, QueryResult } from './types';
 
@@ -16,8 +14,6 @@ export class PostgreSQL implements DBStrategy {
 	}
 
 	async init(): Promise<void> {
-		if (!browser) return;
-
 		try {
 			this.db = await PGlite.create(this.dbOptions);
 		} catch (error) {
@@ -26,7 +22,7 @@ export class PostgreSQL implements DBStrategy {
 					? error
 					: new Error(`Database initialization failed: ${String(error)}`);
 
-			if (dev) console.error('Database initialization error:', initError);
+			console.error('Database initialization error:', initError);
 
 			throw initError;
 		}
@@ -42,8 +38,6 @@ export class PostgreSQL implements DBStrategy {
 			await this.db.transaction(async (tx) => {
 				data = await tx.exec(query);
 			});
-
-			if (dev) console.log(data);
 
 			return {
 				data,
@@ -63,7 +57,5 @@ export class PostgreSQL implements DBStrategy {
 
 	async close(): Promise<void> {
 		await this.db.close();
-
-		if (this.db.closed && dev) console.log('Database: ' + this.dbName + ' is closed!');
 	}
 }
