2d3ee8f1f846e817e86325919dc8ae0d1aa68c62
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-30 16:15:16 -0500

    feat: ✨ add DBWorker Service into main page
    replaces the old store based system with shared port

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,32 @@
 <script lang="ts">
-	import AvailableDBs from '$lib/components/AvailableDBs.svelte';
-	import { dbWorkerMessagesStore, dbWorkerResponsesStore } from '$lib/db/worker/store';
+	import { DBWorkerService } from '$lib/db/worker/service';
 	import type { DBWorkerResponse } from '$lib/db/worker/types';
 
-	import { onDestroy, onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-	let sharedWorker: SharedWorker | undefined;
-	let port: MessagePort | undefined;
 	let loading: boolean = $state(true);
+
+	let dbWorkerService: DBWorkerService;
 
 	onMount(() => {
 		try {
-			sharedWorker = new SharedWorker(new URL('$lib/db/worker/index.ts', import.meta.url), {
-				type: 'module'
+			dbWorkerService = DBWorkerService.getInstance();
+
+			const unsubscribe = dbWorkerService.responses.subscribe((response: DBWorkerResponse) => {
+				if (response.status === 'INITIALIZING') {
+					loading = true;
+				} else if (response.status === 'INITIALIZED') {
+					loading = false;
+				}
 			});
 
-			port = sharedWorker.port;
-			port.start();
-
-			port.onmessage = (event: MessageEvent<DBWorkerResponse>) => {
-				if (event.data.status === 'READY') loading = false;
-				dbWorkerResponsesStore.set(event.data);
+			return () => {
+				unsubscribe();
+				dbWorkerService.disconnect();
 			};
-
-			dbWorkerMessagesStore.subscribe(message => {
-				if (!port) return;
-				port.postMessage(message);
-			})
-
-
 		} catch (e) {
 			console.error(e instanceof Error ? e.message : 'Failed to start DB Worker!');
 		}
-	});
-
-	onDestroy(() => {
-		port?.close();
 	});
 </script>
 
@@ -44,10 +35,5 @@
 		<p>Loading Notebook</p>
 	{:else}
 		<h1 class="mb-6 text-center text-2xl font-bold">Database Manager</h1>
-
-		<div class="grid grid-cols-2 gap-4">
-			<AvailableDBs />
-		</div>
-
 	{/if}
 </div>
