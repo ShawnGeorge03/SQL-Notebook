7968e52e2d4ad9382386f538bb0946a20443bb16
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-11-30 16:31:08 -0500

    revert: 🔥 removing old worker stores
    replaced with db worker service

--- src/lib/db/worker/store.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-import type { DBWorkerMessage, DBWorkerResponse } from './types';
-
-const dbWorkerMessagesStore = writable<DBWorkerMessage>();
-
-const dbWorkerResponsesStore = writable<DBWorkerResponse>();
-
-export { dbWorkerMessagesStore, dbWorkerResponsesStore };
