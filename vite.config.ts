de823a3b05ed559c85c8f9fdf2f67127daa7dc8f
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-12-02 10:39:01 -0500

    feat: ⚗️ create vite plugin for DB Worker
    auto restarts server if DB Worker logic is updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
+
+const DBWorkerWatcher = (): PluginOption => {
+	return {
+		name: 'db-worker-watcher',
+		enforce: 'post',
+		handleHotUpdate({ file, server }) {
+			if (file.endsWith('worker/index.ts'))
+				server.restart();
+		}
+
+	}
+}
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), DBWorkerWatcher()],
 	worker: {
 		format: 'es'
 	},
