5cc2af28586a6b5b760a91f78e99aff29e8350ac
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-12-05 15:14:14 -0500

    feat: ✨ created a editor settings component
    moved code from main page to a component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-	import editorConfig, {
-		updateHighlightTrailingWhitespace,
-		updateHighlightWhitespace,
-		updateLineNumbers
-	} from '$lib/components/Blocks/Block/store';
+	import EditorSettings from '$lib/components/Blocks/EditorSettings.svelte';
 	import TextBlock from '$lib/components/Blocks/TextBlock.svelte';
 	import PostgreSQL from '$lib/components/Examples/PostgreSQL.svelte';
 
@@ -11,37 +7,12 @@
 </script>
 
 <div class="m-36 min-h-screen">
+	<EditorSettings />
 	<PostgreSQL />
 
 	<!-- TextBlock Section for Markdown Input -->
 	<div class="mt-10">
 		<p>Markdown Content:</p>
 		<TextBlock class="markdown-editor" type="markdown" bind:content={markdownContent} />
-	</div>
-
-	<div class="mt-10">
-		<p>Code Editor Settings</p>
-		<div class="flex items-center justify-center gap-10">
-			<button
-				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
-				onclick={() => updateLineNumbers()}
-			>
-				{$editorConfig.lineNumbers ? 'Remove line numbers' : 'Add line numbers'}
-			</button>
-			<button
-				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
-				onclick={() => updateHighlightWhitespace()}
-			>
-				{$editorConfig.highlightWhitespace ? 'Unhighlight Whitespace' : 'Highlight Whitespace'}
-			</button>
-			<button
-				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
-				onclick={() => updateHighlightTrailingWhitespace()}
-			>
-				{$editorConfig.highlightTrailingWhitespace
-					? 'Unhighlight Trailing Whitespace'
-					: 'Highlight Trailing Whitespace'}
-			</button>
-		</div>
 	</div>
 </div>
