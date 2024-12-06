0011990836012772eb48a1d877c6b0057bc91f44
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-12-06 02:02:35 -0500

    feat: ✨ created DuckDB Svelte Component
    ability to run multiple databases, run sample queries

---
+++ src/lib/components/Examples/DuckDB.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import { DuckDB } from '$lib/db/engines/duckdb';
+	import { onMount } from 'svelte';
+	import CodeBlock from '../Blocks/CodeBlock.svelte';
+
+	const DBS = $state<Record<string, DuckDB>>({});
+	let selectedDB = $state('');
+	let query = $state('');
+	let result = $state('');
+
+	onMount(() => {
+		DBS['A'] = new DuckDB();
+		DBS['B'] = new DuckDB();
+		DBS['C'] = new DuckDB();
+		selectedDB = 'A';
+	});
+
+	const init = async () => {
+		try {
+			await DBS[selectedDB].init();
+			result += `>> Successfully created DB: ${selectedDB}\n`;
+		} catch (error) {
+			result += (error instanceof Error ? error.message : 'Failed to initialize database') + '\n';
+		}
+	};
+
+	const execQuery = async () => {
+		const results = await DBS[selectedDB].exec(query);
+
+		if (results.error) {
+			result += `>> Error: ${JSON.stringify(results.error)}\n`;
+		} else if (results.data) {
+			result += `>> ${JSON.stringify(results.data)} (Took: ${results.elapsed} ms)\n`;
+		}
+	};
+
+	const loadSampleQuery = async () => {
+		query =
+			"CREATE TABLE new_tbl AS SELECT * FROM read_json_auto('https://api.datamuse.com/words?ml=sql');\n" +
+			'SELECT * FROM new_tbl;';
+	};
+
+	const close = async () => {
+		await DBS[selectedDB].close();
+		delete DBS[selectedDB];
+		result += `>> Successfully closed DB: ${selectedDB}\n`;
+	};
+</script>
+
+<div class="flex flex-col gap-5 rounded-xl border-4 border-black p-10 shadow-xl">
+	<h2 class="text-center text-2xl">DuckDB Database</h2>
+
+	<div>
+		<h3 class="pb-2 text-xl">Database</h3>
+		<select
+			bind:value={selectedDB}
+			class="mt-1 block rounded-md border-gray-300 shadow-sm sm:text-sm"
+		>
+			{#each Object.keys(DBS) as dbName}
+				<option value={dbName}>{dbName}</option>
+			{/each}
+		</select>
+	</div>
+
+	<div class="flex flex-col">
+		<h3 class="pb-2 text-xl">Query</h3>
+		<CodeBlock type="standard" bind:content={query} />
+	</div>
+	<div>
+		<h3 class="pb-2 text-xl">Result</h3>
+		<div
+			class="h-36 {result.length !== 0
+				? 'overflow-y-scroll'
+				: ''} whitespace-pre-wrap rounded-sm border-2 border-black p-2 dark:border-white"
+		>
+			{result}
+		</div>
+	</div>
+
+	<div class="mt-10">
+		<h3 class="pb-2 text-xl">Database Settings</h3>
+		<div class="flex items-center justify-center gap-10">
+			<button
+				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
+				onclick={() => init()}
+			>
+				Connect to DB
+			</button>
+			<button
+				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
+				onclick={() => loadSampleQuery()}
+			>
+				Load Sample
+			</button>
+			<button
+				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
+				onclick={() => execQuery()}
+			>
+				Run Query
+			</button>
+			<button
+				class="cursor-pointer rounded border-none bg-cyan-700 px-4 py-2 text-white hover:bg-cyan-900"
+				onclick={() => close()}
+			>
+				Close DB
+			</button>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import EditorSettings from '$lib/components/Blocks/EditorSettings.svelte';
 	import TextBlock from '$lib/components/Blocks/TextBlock.svelte';
+	import DuckDB from '$lib/components/Examples/DuckDB.svelte';
 	import PostgreSQL from '$lib/components/Examples/PostgreSQL.svelte';
 
 	let markdownContent = $state('');
 </script>
 
-<div class="m-36 min-h-screen">
+<div class="mx-36 mt-10 min-h-screen">
 	<EditorSettings />
-	<PostgreSQL />
+	<div class="flex flex-col gap-10">
+		<PostgreSQL />
+		<DuckDB />
+	</div>
 	<div class="mt-10 rounded-xl border-4 border-black p-10 shadow-xl">
 		<h2 class="mb-5 text-center text-2xl">Markdown Content</h2>
 		<TextBlock class="markdown-editor" type="markdown" bind:content={markdownContent} />
