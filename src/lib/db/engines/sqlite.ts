c3bf2707e2ba92362a2ff47c719dc85288734c34
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-12-28 03:07:22 -0500

    refactor: ♻️ split `SQLite` exec func

--- src/lib/db/engines/sqlite.ts
+++ src/lib/db/engines/sqlite.ts
@@ -4,6 +4,8 @@ import IDBBatchAtomicVFS from './thrid-party/wa-sqlite/IDBBatchAtomicVFS.js';
 import MemoryAsyncVFS from './thrid-party/wa-sqlite/MemoryAsyncVFS.js';
 import { NotebookType, type DBOptions, type DBStrategy, type QueryResult } from './types';
 
+type ColTypes = Record<string, Record<NotebookType, number>>;
+
 /** Class representing a SQLite Database.
  *
  * @class
@@ -62,85 +64,104 @@ export class SQLite implements DBStrategy {
      * @returns {Promise{QueryResult[]}} - Promise Object reprsenting the query results.
      */
     async exec(query: string): Promise<QueryResult[]> {
+        if (!this.#db)
+            throw new Error('Database not initialized');
+
         try {
-            if (!this.#db)
-                throw new Error('Database not initialized');
-
-            const data: QueryResult[] = [];
-
+            let data: QueryResult[] = [];
             await this.#sqlite3.exec(this.#db, 'BEGIN TRANSACTION;')
-
-            for await (const stmt of this.#sqlite3.statements(this.#db, query)) {
-                const rows = [];
-                const colNames = this.#sqlite3.column_names(stmt);
-                const colTypes: Record<string, Record<string, number>> =
-                    colNames.reduce((acc, cur) => ({ ...acc, [cur]: {} }), {});
-
-                while (await this.#sqlite3.step(stmt) === WaSQLite.SQLITE_ROW) {
-                    const row = this.#sqlite3.row(stmt);
-                    rows.push(row);
-                    let col_num = 0;
-
-                    while (col_num !== Object.keys(colTypes).length) {
-                        const colName = colNames[col_num];
-                        const colType = this.#columnToTypes(await this.#sqlite3.column_type(stmt, col_num))
-
-                        if (!Object.keys(colTypes).includes(colName))
-                            colTypes[colName] = {};
-
-                        if (!Object.keys(colTypes[colName]).includes(colType))
-                            colTypes[colName] = {
-                                ...colTypes[colName],
-                                [colType]: 0,
-                            }
-
-                        colTypes[colName][colType] += 1;
-                        col_num += 1;
-                    }
-                }
-
-                const cols = []
-
-                for (const colName of colNames) {
-                    const colType = colTypes[colName];
-                    let maxKey, maxValue = -Infinity;
-
-                    for (const type in colType) {
-                        if (colType[type] > maxValue) {
-                            maxKey = type;
-                            maxValue = colType[type];
-                        }
-                    }
-
-                    cols.push({
-                        name: colName,
-                        type: maxKey
-                    })
-                }
-
-                data.push({ rows, cols: cols as QueryResult['cols'] });
-            }
-
-            await this.#sqlite3.exec(this.#db, 'COMMIT;')
-
-            return data;
+                .then(async () => data = await this.executeQuery(query));
+            return await this.#sqlite3.exec(this.#db, 'COMMIT;')
+                .then(() => data);
         } catch (error) {
-            await this.#sqlite3.exec(this.#db, 'ROLLBACK;')
-
+            await this.#sqlite3.exec(this.#db, 'ROLLBACK;');
             if (error instanceof Error) {
-                if (error.cause) {
-                    throw new Error(error.message + ' (' + error.cause + ')');
-                } else {
-                    throw new Error(error.message);
-                }
-            } else {
-                throw new Error(`Database query failed: ${String(error)}`);
+                throw new Error(error.cause
+                    ? `${error.message} (${error.cause})`
+                    : error.message
+                );
             }
+            throw new Error(`Database query failed: ${String(error)}`);
         }
     }
 
-    #columnToTypes(field: number) {
-        switch (field) {
+    /**
+     * Executes a SQL query and returns the results.
+     *
+     * @param {string} query - The SQL query to be executed.
+     *
+     * @returns {Promise{QueryResult[]}} - A Promise that resolves to an array of
+     * `QueryResult` objects representing the results of the executed query.
+     */
+    private async executeQuery(query: string): Promise<QueryResult[]> {
+        const data: QueryResult[] = [];
+
+        for await (const statement of this.#sqlite3.statements(this.#db, query)) {
+            const colNames = this.#sqlite3.column_names(statement);
+            const { rows, colTypes } = await this.executeStatement(statement, colNames);
+            if (rows.length > 0 || colNames.length > 0)
+                data.push({ rows, cols: this.getColumnMetadata(colNames, colTypes) });
+        }
+
+        return data;
+    }
+
+    /**
+     * Executes a SQL statement and returns the results.
+     *
+     * @param {number} statement - The prepared SQLite statement.
+     * @param {srting[]} colNames - The column names.
+     *
+     * @returns {Promise<{ rows: unknown[], colTypes: ColTypes }>} The results of the query.
+     */
+    private async executeStatement(statement: number, colNames: string[]) {
+        const rows: unknown[] = [];
+        const colTypes: ColTypes = colNames.reduce((acc, cur) => ({ ...acc, [cur]: {} }), {});
+
+        while (await this.#sqlite3.step(statement) === WaSQLite.SQLITE_ROW) {
+            rows.push(this.#sqlite3.row(statement));
+            for (let i = 0; i < colNames.length; i++) {
+                const colName = colNames[i];
+                const sqliteColType = await this.#sqlite3.column_type(statement, i);
+                const notebookColType = this.mapSQLiteTypesToNotebookTypes(sqliteColType);
+                colTypes[colName][notebookColType] = (colTypes[colName][notebookColType] || 0) + 1;
+            }
+        }
+
+        return { rows, colTypes };
+    }
+
+    /**
+     * Returns the column metadata for a query result.
+     *
+     * @param {srting[]} colNames - The column names.
+     * @param {ColTypes} colTypes - The column types.
+     *
+     * @returns {QueryResult['cols']} The column metadata.
+     */
+    private getColumnMetadata(colNames: string[], colTypes: ColTypes): QueryResult['cols'] {
+        return colNames.map(name => {
+            const typeFrequencies = colTypes[name];
+
+            const [maxType] = Object.entries(typeFrequencies).reduce(
+                ([currentType, currentCount], [type, count]) =>
+                    count > currentCount ? [type, count] : [currentType, currentCount],
+                [NotebookType.STRING, -Infinity]
+            );
+
+            return { name, type: maxType as NotebookType };
+        });
+    }
+
+    /**
+     * Maps SQLite column types to Notebook types.
+     *
+     * @param {number} colType - The SQLite column type to be mapped.
+     *
+     * @returns {NotebookType} The mapped Notebook type.
+     */
+    private mapSQLiteTypesToNotebookTypes(colType: number) {
+        switch (colType) {
             case WaSQLite.SQLITE_NULL:
                 return NotebookType.BOOLEAN
             case WaSQLite.SQLITE_INTEGER:
