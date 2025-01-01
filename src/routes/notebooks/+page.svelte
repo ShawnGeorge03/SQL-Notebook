bbd15fad5b29043fb5cc87a1cdf9755560fc2f31
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-01 02:45:19 -0500

    chore: ⬆️ upgraded `shadcn-svelte` (#33)
    * chore: ⬆️ updated `shadcn-svelte`
    
    uses the next version
    
    * feat: 💄 added `shadcn` Sidebar and other components
    
    added `Button`, `Input`, `Separator`, `Sheet`, `Skeleton`, `Tooltip`
    
    * feat: ✨ created simple sidebar

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -1,13 +1,22 @@
-<div class="grid grid-cols-1 md:grid-cols-[auto_1fr_auto]">
-	<aside class="sticky top-0 col-span-1 h-screen bg-yellow-500 px-20 py-4">(sidebar [left])</aside>
-	<main class="space-y-4 bg-green-500">
-		<header class="sticky top-0 z-10 bg-red-500 p-4">(header)</header>
-		<p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
-		<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
-		<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
-		<p class="h-[512px] bg-purple-500 p-4">Paragraph 4</p>
-		<p class="h-[512px] bg-purple-500 p-4">Paragraph 5</p>
-		<footer class="sticky bottom-0 z-10 bg-blue-500 p-4">(footer)</footer>
-	</main>
-	<!-- <aside class="bg-yellow-500 py-4 px-20">(sidebar [right])</aside> -->
-</div>
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import SidebarLeft from '$lib/components/ui/sidebar/sidebar-left.svelte';
+</script>
+
+<Sidebar.Provider>
+	<SidebarLeft />
+	<Sidebar.Inset>
+		<main class="space-y-4 bg-green-500">
+			<header class="sticky top-0 z-10 bg-red-500 p-4">
+				<Sidebar.Trigger />
+			</header>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 4</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 5</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 6</p>
+			<footer class="sticky bottom-0 z-10 bg-blue-500 p-4">(footer)</footer>
+		</main>
+	</Sidebar.Inset>
+</Sidebar.Provider>
