69c4d3eb91393850bb7b1cac6ad88e5f0cd0532e
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-02 15:55:23 -0500

    feat: ✨ created `ThemeToggle`
    used to toggle between themes

---
+++ src/lib/components/ui/ThemeToggle.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Moon, Sun } from 'lucide-svelte';
+
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { cn } from '$lib/utils';
+	import { resetMode, setMode } from 'mode-watcher';
+
+	const { class: className } = $props();
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger
+		class={cn(buttonVariants({ variant: 'outline' }), className, 'h-7 w-7 p-0')}
+	>
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content align="end">
+		<DropdownMenu.Item onclick={() => setMode('light')}>Light</DropdownMenu.Item>
+		<DropdownMenu.Item onclick={() => setMode('dark')}>Dark</DropdownMenu.Item>
+		<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
