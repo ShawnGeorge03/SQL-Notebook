ebd8f5273efd393d700275e3ba70de1ea4741247
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-03 20:11:59 -0500

    refactor: ♻️ removed extra `main` tag

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -7,22 +7,20 @@
 
 <Sidebar.Provider>
 	<SidebarLeft />
-	<Sidebar.Inset>
-		<main class="space-y-4 bg-green-500">
-			<header class="sticky top-0 z-10 bg-red-500 p-4">
-				<Sidebar.Trigger />
-				<div class="float-right flex justify-end gap-4">
-					<SettingsModal />
-					<ThemeToggle />
-				</div>
-			</header>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 4</p>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 5</p>
-			<p class="h-[512px] bg-purple-500 p-4">Paragraph 6</p>
-			<footer class="sticky bottom-0 z-10 bg-blue-500 p-4">(footer)</footer>
-		</main>
+	<Sidebar.Inset class="space-y-4 bg-green-500">
+		<header class="sticky top-0 z-10 bg-red-500 p-4">
+			<Sidebar.Trigger />
+			<div class="float-right flex justify-end gap-4">
+				<SettingsModal />
+				<ThemeToggle />
+			</div>
+		</header>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 4</p>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 5</p>
+		<p class="h-[512px] bg-purple-500 p-4">Paragraph 6</p>
+		<footer class="sticky bottom-0 z-10 bg-blue-500 p-4">(footer)</footer>
 	</Sidebar.Inset>
 </Sidebar.Provider>
