a88c526ba873628aefbef23385f71f87bb13f56f
Aditya Kulkarni <adityaskulkarni23@gmail.com>
2025-01-06 15:35:49 +0530

    feat: ✨ Implement notebook creation dialog and enhance database actions

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -5,17 +5,18 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Badge } from '$lib/components/ui/badge/index.js';
 
-	import { RotateCw, Plus, CircleDot, MinusSquare } from 'lucide-svelte';
+	import { Plus, CircleDot, Ellipsis } from 'lucide-svelte';
 
 	import SQLiteIcon from '$lib/assets/sqlite.svg?raw';
 	import PostgreSQLIcon from '$lib/assets/postgresql.svg?raw';
 	import databaseIcon from '$lib/assets/database.svg?raw';
+	import Button from './ui/button/button.svelte';
 
-	let notebooks = [
+	let notebooks = $state([
 		{ name: 'Notebook 1', id: 1 },
 		{ name: 'Notebook 2', id: 2 },
 		{ name: 'Notebook 3', id: 3 }
-	];
+	]);
 
 	let databases = $state([
 		{ name: 'Movies', id: 1, engine: 'pgsql', persistent: true, status: 'active' },
@@ -23,11 +24,16 @@
 		{ name: 'Sample-Chinook 2', id: 3, engine: '', persistent: false, status: '' }
 	]);
 
-	// Database Dialog
-	let showDialog = $state(false);
+	// Database Dialogs
+	let showCreateDBDialog = $state(false);
+	let showDBActionsDialog = $state(false);
 	let newDatabaseName = $state('');
 	let databaseType = $state('sqlite');
 	let databasePersistence = $state('yes');
+
+	// Notebook Dialog
+	let showCreateNBDialog = $state(false);
+	let newNotebookName = $state('');
 
 	// Function to get the icon for the database engine
 	function getDatabaseIcon(engine: string) {
@@ -52,9 +58,7 @@
 		openNotebook: (id: number) => performAction('Opening notebook', id),
 		refreshNotebooks: () => performAction('Refreshing notebooks list'),
 		closeDatabase: (id: number) => performAction('Closing database', id),
-		terminateDatabase: (id: number) => performAction('Terminating database', id),
-		getAllDatabases: () => performAction('Fetching all databases'),
-		getActiveDatabases: () => performAction('Fetching active databases')
+		terminateDatabase: (id: number) => performAction('Terminating database', id)
 	};
 
 	function createDatabase() {
@@ -70,9 +74,24 @@
 				}
 			];
 			newDatabaseName = '';
-			showDialog = false;
+			showCreateDBDialog = false;
 		}
 		console.log($state.snapshot(databases));
+	}
+
+	function createNotebook() {
+		if (newNotebookName.trim()) {
+			notebooks = [
+				...notebooks,
+				{
+					name: newNotebookName,
+					id: notebooks.length + 1
+				}
+			];
+			newNotebookName = '';
+			showCreateNBDialog = false;
+		}
+		console.log($state.snapshot(notebooks));
 	}
 </script>
 
@@ -80,15 +99,36 @@
 	<Sidebar.Content class="overflow-hidden">
 		<Sidebar.Group>
 			<Sidebar.GroupLabel>Notebooks</Sidebar.GroupLabel>
-			<Sidebar.GroupAction title="Notebook Actions" class="right-1">
+			<Sidebar.GroupAction title="Create Notebook" class="right-3">
 				{#snippet child({ props })}
-					<button onclick={actions.refreshNotebooks} {...props}>
-						<RotateCw size="15" />
-					</button>
+					<Dialog.Root>
+						<button onclick={() => (showCreateNBDialog = true)} {...props}>
+							<Dialog.Trigger>
+								<Plus size="15" />
+							</Dialog.Trigger>
+						</button>
+						<Dialog.Content>
+							<Dialog.Header>
+								<Dialog.Title>Create Notebook</Dialog.Title>
+							</Dialog.Header>
+							<div class="grid gap-4 py-4">
+								<div class="grid grid-cols-4 items-center gap-4">
+									<label for="Notebook Name" class="text-right">Name</label>
+									<Input
+										type="text"
+										placeholder="Notebook Name"
+										class="col-span-3 rounded-md border border-gray-300 p-2"
+										bind:value={newNotebookName}
+										id="Notebook Name"
+									/>
+								</div>
+							</div>
+							<button class="rounded-md bg-blue-500 p-2 text-white" onclick={createNotebook}>
+								Create
+							</button>
+						</Dialog.Content>
+					</Dialog.Root>
 				{/snippet}
-			</Sidebar.GroupAction>
-			<Sidebar.GroupAction title="Notebook Actions" class="right-7">
-				<Plus size="15" />
 			</Sidebar.GroupAction>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
@@ -111,7 +151,7 @@
 			<Sidebar.GroupAction title="Create Database" class="right-3">
 				{#snippet child({ props })}
 					<Dialog.Root>
-						<button onclick={() => (showDialog = true)} {...props}>
+						<button onclick={() => (showCreateDBDialog = true)} {...props}>
 							<Dialog.Trigger>
 								<Plus size="15" />
 							</Dialog.Trigger>
@@ -119,46 +159,46 @@
 						<Dialog.Content>
 							<Dialog.Header>
 								<Dialog.Title>Create Database</Dialog.Title>
-								<div class="grid gap-4 py-4">
-									<div class="grid grid-cols-4 items-center gap-4">
-										<label for="Database Name" class="text-right">Name</label>
-										<Input
-											type="text"
-											placeholder="Database Name"
-											class="col-span-3 rounded-md border border-gray-300 p-2"
-											bind:value={newDatabaseName}
-											id="database name"
-										/>
-									</div>
-									<div class="grid grid-cols-4 items-center gap-4">
-										<label for="Database Type" class="text-right">Type</label>
-										<Select.Root type="single" bind:value={databaseType}>
-											<Select.Trigger class="w-[180px]"
-												>{$state.snapshot(databaseType)}</Select.Trigger
-											>
-											<Select.Content>
-												<Select.Item value="pgsql">pgsql</Select.Item>
-												<Select.Item value="sqlite">sqlite</Select.Item>
-											</Select.Content>
-										</Select.Root>
-									</div>
-									<div class="grid grid-cols-4 items-center gap-4">
-										<label for="Database persistence">Presistence</label>
-										<Select.Root type="single" bind:value={databasePersistence}>
-											<Select.Trigger class="w-[180px]"
-												>{$state.snapshot(databasePersistence)}</Select.Trigger
-											>
-											<Select.Content>
-												<Select.Item value="Yes">Yes</Select.Item>
-												<Select.Item value="No">No</Select.Item>
-											</Select.Content>
-										</Select.Root>
-									</div>
-								</div>
-								<button class="mt-2 rounded-md bg-blue-500 p-2 text-white" onclick={createDatabase}>
-									Create
-								</button>
 							</Dialog.Header>
+							<div class="grid gap-4 py-4">
+								<div class="grid grid-cols-4 items-center gap-4">
+									<label for="Database Name" class="text-right">Name</label>
+									<Input
+										type="text"
+										placeholder="Database Name"
+										class="col-span-3 rounded-md border border-gray-300 p-2"
+										bind:value={newDatabaseName}
+										id="Database Name"
+									/>
+								</div>
+								<div class="grid grid-cols-4 items-center gap-4">
+									<label for="Database Type" class="text-right">Type</label>
+									<Select.Root type="single" bind:value={databaseType}>
+										<Select.Trigger class="w-[180px]"
+											>{$state.snapshot(databaseType)}</Select.Trigger
+										>
+										<Select.Content id="Database Type">
+											<Select.Item value="pgsql">pgsql</Select.Item>
+											<Select.Item value="sqlite">sqlite</Select.Item>
+										</Select.Content>
+									</Select.Root>
+								</div>
+								<div class="grid grid-cols-4 items-center gap-4">
+									<label for="Database persistence" class="text-right">Presistent</label>
+									<Select.Root type="single" bind:value={databasePersistence}>
+										<Select.Trigger class="w-[180px]"
+											>{$state.snapshot(databasePersistence)}</Select.Trigger
+										>
+										<Select.Content id="Database persistence">
+											<Select.Item value="Yes">Yes</Select.Item>
+											<Select.Item value="No">No</Select.Item>
+										</Select.Content>
+									</Select.Root>
+								</div>
+							</div>
+							<button class="mt-2 rounded-md bg-blue-500 p-2 text-white" onclick={createDatabase}>
+								Create
+							</button>
 						</Dialog.Content>
 					</Dialog.Root>
 				{/snippet}
@@ -184,7 +224,41 @@
 								{/if}
 							</Sidebar.MenuButton>
 							<Sidebar.MenuAction>
-								<MinusSquare /> <span class="sr-only">Close Database</span>
+								{#snippet child({ props })}
+									<Dialog.Root>
+										<button onclick={() => (showDBActionsDialog = true)} {...props}>
+											<Dialog.Trigger>
+												<Ellipsis size="15" />
+											</Dialog.Trigger>
+											<Dialog.Content>
+												<Dialog.Header>
+													<Dialog.Title>Database Actions</Dialog.Title>
+													<div class="my-2 border-t border-gray-200">
+														<div class="grid gap-4 py-4">
+															<p class="text-grey">Database Name: {database.name}</p>
+															<p class="text-grey">Database Engine: {database.engine}</p>
+															<p class="text-grey">Database Status: {database.status}</p>
+														</div>
+													</div></Dialog.Header
+												>
+												<Button
+													class="w-full text-center"
+													onclick={() => actions.closeDatabase(database.id)}
+												>
+													Close
+												</Button>
+												{#if database.status === 'active'}
+													<Button
+														class="w-full p-2 text-center"
+														onclick={() => actions.terminateDatabase(database.id)}
+													>
+														Terminate
+													</Button>
+												{/if}
+											</Dialog.Content>
+										</button>
+									</Dialog.Root>
+								{/snippet}
 							</Sidebar.MenuAction>
 						</Sidebar.MenuItem>
 					{/each}
