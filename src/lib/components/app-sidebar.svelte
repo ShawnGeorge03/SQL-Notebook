fcc012419100ac649db5228a871fa4716964c6aa
Aditya Kulkarni <adityaskulkarni23@gmail.com>
2025-01-06 15:04:28 +0530

    feat: ✨ Add Dialog to create a database

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { RotateCw, Plus, Database, CircleDot, MinusSquare } from 'lucide-svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+
+	import { RotateCw, Plus, CircleDot, MinusSquare } from 'lucide-svelte';
+
 	import SQLiteIcon from '$lib/assets/sqlite.svg?raw';
 	import PostgreSQLIcon from '$lib/assets/postgresql.svg?raw';
 	import databaseIcon from '$lib/assets/database.svg?raw';
-	import { Badge } from '$lib/components/ui/badge/index.js';
 
 	let notebooks = [
 		{ name: 'Notebook 1', id: 1 },
@@ -12,11 +17,17 @@
 		{ name: 'Notebook 3', id: 3 }
 	];
 
-	let databases = [
+	let databases = $state([
 		{ name: 'Movies', id: 1, engine: 'pgsql', persistent: true, status: 'active' },
 		{ name: 'Sample-Chinook', id: 2, engine: 'sqlite', persistent: true, status: 'inactive' },
 		{ name: 'Sample-Chinook 2', id: 3, engine: '', persistent: false, status: '' }
-	];
+	]);
+
+	// Database Dialog
+	let showDialog = $state(false);
+	let newDatabaseName = $state('');
+	let databaseType = $state('sqlite');
+	let databasePersistence = $state('yes');
 
 	// Function to get the icon for the database engine
 	function getDatabaseIcon(engine: string) {
@@ -40,13 +51,29 @@
 	const actions = {
 		openNotebook: (id: number) => performAction('Opening notebook', id),
 		refreshNotebooks: () => performAction('Refreshing notebooks list'),
-		createDatabase: () => performAction('Adding a new database'),
-		refreshDatabase: () => performAction('Loading database'),
 		closeDatabase: (id: number) => performAction('Closing database', id),
 		terminateDatabase: (id: number) => performAction('Terminating database', id),
 		getAllDatabases: () => performAction('Fetching all databases'),
 		getActiveDatabases: () => performAction('Fetching active databases')
 	};
+
+	function createDatabase() {
+		if (newDatabaseName.trim()) {
+			databases = [
+				...databases,
+				{
+					name: newDatabaseName,
+					id: databases.length + 1,
+					engine: databaseType,
+					persistent: databasePersistence === 'Yes',
+					status: 'inactive'
+				}
+			];
+			newDatabaseName = '';
+			showDialog = false;
+		}
+		console.log($state.snapshot(databases));
+	}
 </script>
 
 <Sidebar.Root collapsible="none" side="left">
@@ -81,11 +108,60 @@
 		</Sidebar.Group>
 		<Sidebar.Group>
 			<Sidebar.GroupLabel>Databases</Sidebar.GroupLabel>
-			<Sidebar.GroupAction title="Database Actions" class="right-1">
-				<RotateCw size="15" />
-			</Sidebar.GroupAction>
-			<Sidebar.GroupAction title="Database Actions" class="right-7">
-				<Plus size="15" />
+			<Sidebar.GroupAction title="Create Database" class="right-3">
+				{#snippet child({ props })}
+					<Dialog.Root>
+						<button onclick={() => (showDialog = true)} {...props}>
+							<Dialog.Trigger>
+								<Plus size="15" />
+							</Dialog.Trigger>
+						</button>
+						<Dialog.Content>
+							<Dialog.Header>
+								<Dialog.Title>Create Database</Dialog.Title>
+								<div class="grid gap-4 py-4">
+									<div class="grid grid-cols-4 items-center gap-4">
+										<label for="Database Name" class="text-right">Name</label>
+										<Input
+											type="text"
+											placeholder="Database Name"
+											class="col-span-3 rounded-md border border-gray-300 p-2"
+											bind:value={newDatabaseName}
+											id="database name"
+										/>
+									</div>
+									<div class="grid grid-cols-4 items-center gap-4">
+										<label for="Database Type" class="text-right">Type</label>
+										<Select.Root type="single" bind:value={databaseType}>
+											<Select.Trigger class="w-[180px]"
+												>{$state.snapshot(databaseType)}</Select.Trigger
+											>
+											<Select.Content>
+												<Select.Item value="pgsql">pgsql</Select.Item>
+												<Select.Item value="sqlite">sqlite</Select.Item>
+											</Select.Content>
+										</Select.Root>
+									</div>
+									<div class="grid grid-cols-4 items-center gap-4">
+										<label for="Database persistence">Presistence</label>
+										<Select.Root type="single" bind:value={databasePersistence}>
+											<Select.Trigger class="w-[180px]"
+												>{$state.snapshot(databasePersistence)}</Select.Trigger
+											>
+											<Select.Content>
+												<Select.Item value="Yes">Yes</Select.Item>
+												<Select.Item value="No">No</Select.Item>
+											</Select.Content>
+										</Select.Root>
+									</div>
+								</div>
+								<button class="mt-2 rounded-md bg-blue-500 p-2 text-white" onclick={createDatabase}>
+									Create
+								</button>
+							</Dialog.Header>
+						</Dialog.Content>
+					</Dialog.Root>
+				{/snippet}
 			</Sidebar.GroupAction>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
