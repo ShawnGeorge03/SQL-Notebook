956d0285a7a9ac255ad3493d184addaf33441685
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-06 15:45:51 -0500

    refactor: 🚚 renamed Create Cell `ButtonGroup`

---
+++ src/lib/components/Notebook/CreateCell/ButtonGroup.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import CreateMarkdownCell from './CreateMarkdownCell.svelte';
+	import CreateQueryCell from './CreateQueryCell.svelte';
+	import { type CreateCellBaseProps } from './type';
+
+	let open = $state(false);
+	let { class: className, position, addNewCell }: CreateCellBaseProps = $props();
+</script>
+
+<div class={cn('m-auto flex gap-4', className)} role="group">
+	<CreateQueryCell class="border border-gray-200" {position} {addNewCell} />
+	<CreateMarkdownCell class="border border-gray-200" {position} {addNewCell} />
+</div>
