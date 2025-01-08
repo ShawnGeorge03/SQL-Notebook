4143ad64f1dc7968c9a849c642c7902994f2b4d6
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-08 13:27:34 -0500

    feat: 💄 updated `CreateMarkdownCell` height
    matches the height of the other `CreateCell`

--- src/lib/components/Notebook/CreateCell/CreateMarkdownCell.svelte
+++ src/lib/components/Notebook/CreateCell/CreateMarkdownCell.svelte
@@ -13,7 +13,7 @@
 	aria-label="Create Markdown Cell"
 	onclick={() => addNewCell(position, { cellType: 'markdown' })}
 	class={cn(
-		'inline-flex h-fit items-center bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white',
+		'inline-flex h-auto items-center bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white',
 		className
 	)}
 >
