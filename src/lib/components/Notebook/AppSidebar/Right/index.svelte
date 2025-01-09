fe5f71ab2ebc071cbab66e8e2a899a3ba3d4758b
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-09 16:35:07 -0500

    feat: ✨ added the base of sidebar
    general layout and responsiveness work

---
+++ src/lib/components/Notebook/AppSidebar/Right/index.svelte
@@ -0,0 +1,5 @@
+<aside
+	class="sticky top-0 col-span-1 h-screen w-[250px] bg-sidebar py-4 pl-10 text-sidebar-foreground max-sm:hidden"
+>
+	(sidebar [right])
+</aside>
--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as AppSidebar from '$lib/components/Notebook/AppSidebar/';
 	import { Markdown, Query } from '$lib/components/Notebook/Cells';
 	import * as CreateCell from '$lib/components/Notebook/CreateCell/index';
 	import type { CellMetadata } from '$lib/components/Notebook/CreateCell/type';
@@ -105,46 +106,52 @@ ORDER BY
 	};
 </script>
 
-<header class="sticky top-0 z-20 bg-red-500 p-4">
-	<div class="float-left flex justify-end gap-4">
-		<CreateCell.QueryCell position={cells.length} {addNewCell} />
-		<CreateCell.MarkdownCell position={cells.length} {addNewCell} />
-	</div>
-	<div class="float-right flex justify-end gap-4">
-		<Notifications />
-		<Settings />
-		<ThemeToggle />
-	</div>
-</header>
-<div class="flex flex-col items-center justify-center">
-	<CreateCell.ButtonGroup
-		class="w-[400px] py-5 transition-[width] duration-300 ease-in-out md:w-[500px] lg:w-[700px] xl:w-[1000px]"
-		position={0}
-		{addNewCell}
-	/>
-	{#each cells as cell, i (cell.id)}
-		{#if cell.cellType === 'markdown'}
-			<Markdown class="py-4" bind:content={cell.content.text} />
-		{:else if cell.cellType === 'query'}
-			<Query
-				class="py-4"
-				position={i}
-				id={cell.id}
-				bind:name={cell.content.name}
-				bind:query={cell.content.query}
-				bind:dbName={cell.content.dbName}
-				bind:engine={cell.content.engine}
-				{moveUpCell}
-				{moveDownCell}
-				{copyCell}
-				{removeCell}
+<div class="grid grid-cols-1 sm:grid-cols-[auto_1fr]">
+	<AppSidebar.Left />
+	<main class="bg-green-500">
+		<header class="sticky top-0 z-20 bg-red-500 p-4 pb-14">
+			<div class="float-left flex justify-end gap-4">
+				<CreateCell.QueryCell position={cells.length} {addNewCell} />
+				<CreateCell.MarkdownCell position={cells.length} {addNewCell} />
+			</div>
+			<div class="float-right flex justify-end gap-4">
+				<Notifications />
+				<Settings />
+				<ThemeToggle />
+			</div>
+		</header>
+		<div class="flex flex-col items-center justify-center">
+			<CreateCell.ButtonGroup
+				class="w-[400px] py-5 transition-[width] duration-300 ease-in-out md:w-[500px] lg:w-[700px] xl:w-[1000px]"
+				position={0}
+				{addNewCell}
 			/>
-		{/if}
-		<CreateCell.ButtonGroup
-			class={`w-[400px] transition-[width] duration-300 ease-in-out md:w-[500px] lg:w-[700px] xl:w-[1000px] ${i === cells.length - 1 ? 'pb-24' : ''}`}
-			position={i + 1}
-			{addNewCell}
-		/>
-	{/each}
+			{#each cells as cell, i (cell.id)}
+				{#if cell.cellType === 'markdown'}
+					<Markdown class="py-4" bind:content={cell.content.text} />
+				{:else if cell.cellType === 'query'}
+					<Query
+						class="py-4"
+						position={i}
+						id={cell.id}
+						bind:name={cell.content.name}
+						bind:query={cell.content.query}
+						bind:dbName={cell.content.dbName}
+						bind:engine={cell.content.engine}
+						{moveUpCell}
+						{moveDownCell}
+						{copyCell}
+						{removeCell}
+					/>
+				{/if}
+				<CreateCell.ButtonGroup
+					class={`w-[400px] transition-[width] duration-300 ease-in-out md:w-[500px] lg:w-[700px] xl:w-[1000px] ${i === cells.length - 1 ? 'pb-24' : ''}`}
+					position={i + 1}
+					{addNewCell}
+				/>
+			{/each}
+		</div>
+		<footer class="fixed bottom-0 z-20 w-[100%] bg-blue-500 p-4">(footer)</footer>
+	</main>
+	<!-- <AppSidebar.Right /> -->
 </div>
-<footer class="fixed bottom-0 z-20 w-[100%] bg-blue-500 p-4">(footer)</footer>
