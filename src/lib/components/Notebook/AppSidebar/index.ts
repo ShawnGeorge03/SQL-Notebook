2338e5fe9a7b68121d4d9edfd1be6ffee5a8c753
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-09 23:32:21 -0500

    feat: ✨ updated base implementation of Sidebar
    general layout and responsiveness work

---
+++ src/lib/components/Notebook/AppSidebar/index.ts
@@ -0,0 +1,4 @@
+import Left from '$lib/components/Notebook/AppSidebar/Left/index.svelte';
+import Right from '$lib/components/Notebook/AppSidebar/Right/index.svelte';
+
+export { Left, Right };
--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -1,66 +1,15 @@
 <script lang="ts">
+	import * as AppSidebar from '$lib/components/Notebook/AppSidebar/';
 	import { Markdown, Query } from '$lib/components/Notebook/Cells';
 	import * as CreateCell from '$lib/components/Notebook/CreateCell/index';
 	import type { CellMetadata } from '$lib/components/Notebook/CreateCell/type';
 	import Notifications from '$lib/components/Notebook/Header/Notifications.svelte';
 	import Settings from '$lib/components/Notebook/Header/Settings/Modal.svelte';
 	import ThemeToggle from '$lib/components/Notebook/Header/ThemeToggle.svelte';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import SidebarLeft from '$lib/components/ui/sidebar/sidebar-left.svelte';
-	import { DBEngine } from '$lib/db/worker/types';
 	import type { NotebookCell } from '$lib/indexeddb/types';
 	import { nanoid } from 'nanoid/non-secure';
 
-	const cells: NotebookCell[] = $state<NotebookCell[]>([
-		{
-			id: nanoid(),
-			cellType: 'markdown',
-			content: {
-				name: 'Markdown',
-				text: 'Hello World, This is Markdown'
-			}
-		},
-		{
-			id: nanoid(),
-			cellType: 'query',
-			content: {
-				name: 'Query',
-				query: "SELECT 'HELLO WORLD'",
-				engine: DBEngine.PGSQL,
-				dbName: 'PP'
-			}
-		},
-		{
-			id: nanoid(),
-			cellType: 'query',
-			content: {
-				name: 'Query',
-				query: 'SELECT * FROM genre',
-				engine: DBEngine.PGSQL,
-				dbName: 'PP'
-			}
-		},
-		{
-			id: nanoid(),
-			cellType: 'query',
-			content: {
-				name: 'Query',
-				query: `SELECT
-  *
-FROM
-  pg_catalog.pg_type
-WHERE
-  typtype = 'b' OR
-  typtype = 'c' OR
-  typtype = 'p' OR
-  typtype = 'e'
-ORDER BY
-  typname;`,
-				engine: DBEngine.PGSQL,
-				dbName: 'PP'
-			}
-		}
-	]);
+	const cells: NotebookCell[] = $state<NotebookCell[]>([]);
 
 	const addNewCell = (position: number, metadata: CellMetadata) => {
 		const cell = { id: nanoid(), cellType: metadata.cellType, content: {} };
@@ -107,12 +56,11 @@ ORDER BY
 	};
 </script>
 
-<Sidebar.Provider>
-	<SidebarLeft />
-	<Sidebar.Inset class="bg-green-500">
-		<header class="sticky top-0 z-20 bg-red-500 p-4">
+<div class="grid grid-cols-1 sm:grid-cols-[auto_1fr]">
+	<AppSidebar.Left />
+	<main class="bg-green-500">
+		<header class="sticky top-0 z-20 bg-red-500 p-4 pb-14">
 			<div class="float-left flex justify-end gap-4">
-				<Sidebar.Trigger />
 				<CreateCell.QueryCell position={cells.length} {addNewCell} />
 				<CreateCell.MarkdownCell position={cells.length} {addNewCell} />
 			</div>
@@ -122,7 +70,7 @@ ORDER BY
 				<ThemeToggle />
 			</div>
 		</header>
-		<div class="flex flex-col items-center justify-center">
+		<div class="flex h-screen flex-col items-center justify-center">
 			<CreateCell.ButtonGroup
 				class="w-[400px] py-5 transition-[width] duration-300 ease-in-out md:w-[500px] lg:w-[700px] xl:w-[1000px]"
 				position={0}
@@ -154,5 +102,6 @@ ORDER BY
 			{/each}
 		</div>
 		<footer class="fixed bottom-0 z-20 w-[100%] bg-blue-500 p-4">(footer)</footer>
-	</Sidebar.Inset>
-</Sidebar.Provider>
+	</main>
+	<!-- <AppSidebar.Right /> -->
+</div>
