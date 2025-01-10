4bbd83b5873094dcf3d70a8a34d236976fad2b77
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2025-01-09 23:50:07 -0500

    feat: ✨ modified Alert Dialog Action Button
    allows for button variants and sizes

--- src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
-	import { buttonVariants } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+	import { AlertDialog as AlertDialogPrimitive } from 'bits-ui';
+
+	interface AlertDialogActionProps extends AlertDialogPrimitive.ActionProps {
+		variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
+		size?: 'default' | 'sm' | 'lg' | 'icon';
+	}
 
 	let {
 		class: className,
 		ref = $bindable(null),
+		variant = 'default',
+		size = 'default',
 		...restProps
-	}: AlertDialogPrimitive.ActionProps = $props();
+	}: AlertDialogActionProps = $props();
 </script>
 
-<AlertDialogPrimitive.Action bind:ref class={cn(buttonVariants(), className)} {...restProps} />
+<AlertDialogPrimitive.Action
+	bind:ref
+	class={cn(buttonVariants({ variant, size }), className)}
+	{...restProps}
+/>
